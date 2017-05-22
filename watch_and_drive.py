0cde2033764fcbb5b6f8015ac25cc9ea047be96c
Ralph J McIntyre <ralph.j.mcintyre@gmail.com>
2017-05-22 17:35:13 -0400

    Made some variables.

--- watch_and_drive.py
+++ watch_and_drive.py
@@ -7,15 +7,15 @@ mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
-        print "Creating:",
-	cmdstring = "gdrive upload %s" % (event.pathname)
-	os.system(cmdstring)
+        cmdstring = "gdrive upload %s" % (event.pathname)
+        print "Creating:", os.system(cmdstring)
 
     def process_IN_DELETE(self, event):
         print "Removing:", event.pathname
 
 handler = EventHandler()
 notifier = pyinotify.Notifier(wm, handler)
-wdd = wm.add_watch('/tmp', mask, rec=True)
+directory = '/tmp'
+wdd = wm.add_watch(directory, mask, rec=True)
 
 notifier.loop()
